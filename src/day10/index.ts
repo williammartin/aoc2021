773a1ac6b268877021fb81648c30937bacb1fec6
William Martin <will@storyscript.io>
2021-12-10 22:26:07 +0100

    Implement weird pattern matching for Day 10

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -4,20 +4,6 @@ import * as F from "fp-ts/lib/function.js"
 import { splitBy } from "../utils/index.js"
 import { Ord } from "fp-ts/lib/number.js"
 
-const part1Scores = new Map<string, number>([
-  [")", 3],
-  ["]", 57],
-  ["}", 1197],
-  [">", 25137],
-])
-
-const part2Scores = new Map<string, number>([
-  [")", 1],
-  ["]", 2],
-  ["}", 3],
-  [">", 4],
-])
-
 // Totally stolen from https://dev.to/glebirovich/typescript-data-structures-stack-and-queue-hld#stack
 class Stack<T> {
   private storage: T[] = []
@@ -52,13 +38,43 @@ class Stack<T> {
   }
 }
 
-const parseInput = (rawInput: string) =>
-  F.pipe(rawInput, splitBy("\n"), A.map(splitBy("")))
+// Absolutely disgusting no help from the compiler here
+// Just messing about with union types and matches in place
+// of switch cause I'm a bad person.
+type Char = "{" | "(" | "[" | "<" | ">" | "]" | ")" | "}"
+const isChar = (c: string): c is Char =>
+  ["{", "(", "[", "<", ">", "]", ")", "}"].includes(c)
 
-type Failure = { location: number; char: string }
-type Corrupt = { _tag: "corrupt"; chars: string[]; failure: Failure }
-type Incomplete = { _tag: "incomplete"; chars: string[] }
-type Valid = { _tag: "valid"; chars: string[] }
+const parseChar = (c: string): Char => {
+  if (!isChar(c)) {
+    throw new Error(`Failed to parse input char: ${c}`)
+  }
+  return c
+}
+
+const applyCharPartial =
+  <R>(
+    fns: Partial<{
+      [P in Char]: () => R
+    }>,
+    otherwise: (char: Char) => R,
+  ) =>
+  (char: Char): R => {
+    for (const [fnName, fn] of Object.entries(fns)) {
+      if (fnName === char) {
+        return fn()
+      }
+    }
+    return otherwise(char)
+  }
+
+const parseInput = (rawInput: string): Char[][] =>
+  F.pipe(rawInput, splitBy("\n"), A.map(F.flow(splitBy(""), A.map(parseChar))))
+
+type Failure = { location: number; char: Char }
+type Corrupt = { _tag: "corrupt"; chars: Char[]; failure: Failure }
+type Incomplete = { _tag: "incomplete"; chars: Char[] }
+type Valid = { _tag: "valid"; chars: Char[] }
 type ParseResult = Valid | Incomplete | Corrupt
 
 const isIncomplete = (parseResult: ParseResult): parseResult is Incomplete =>
@@ -67,8 +83,9 @@ const isIncomplete = (parseResult: ParseResult): parseResult is Incomplete =>
 const isCorrupt = (parseResult: ParseResult): parseResult is Corrupt =>
   parseResult._tag === "corrupt"
 
-const parseChars = (chars: string[]): ParseResult => {
-  const stack = new Stack<string>()
+const parseChars = (chars: Char[]): ParseResult => {
+  const stack = new Stack<Char>()
+
   for (const [i, char] of chars.entries()) {
     switch (char) {
       case "{":
@@ -107,6 +124,13 @@ const parseChars = (chars: string[]): ParseResult => {
   return { _tag: "valid", chars }
 }
 
+const part1Scores = new Map<Char, number>([
+  [")", 3],
+  ["]", 57],
+  ["}", 1197],
+  [">", 25137],
+])
+
 const part1 = (rawInput: string) =>
   F.pipe(
     rawInput,
@@ -116,8 +140,20 @@ const part1 = (rawInput: string) =>
     A.reduce(0, (acc, curr) => acc + part1Scores.get(curr.failure.char)!),
   )
 
-const missingChars = (chars: string[]): string[] => {
-  const stack = new Stack<string>()
+const findClosingChar = applyCharPartial(
+  {
+    "{": (): Char => "}",
+    "(": (): Char => ")",
+    "[": (): Char => "]",
+    "<": (): Char => ">",
+  },
+  (char) => {
+    throw new Error(`unexpected char ${char}`)
+  },
+)
+
+const missingChars = (chars: Char[]): Char[] => {
+  const stack = new Stack<Char>()
   for (const char of chars) {
     switch (char) {
       case "{":
@@ -135,25 +171,21 @@ const missingChars = (chars: string[]): string[] => {
     }
   }
 
-  return F.pipe(
-    stack.toArray(),
-    A.reverse,
-    A.map((char) => {
-      switch (char) {
-        case "{":
-          return "}"
-        case "(":
-          return ")"
-        case "[":
-          return "]"
-        case "<":
-          return ">"
-        default:
-          throw new Error(`unexpected char ${char}`)
-      }
-    }),
-  )
+  return F.pipe(stack.toArray(), A.reverse, A.map(findClosingChar))
 }
+
+const part2Scores = new Map<Char, number>([
+  [")", 1],
+  ["]", 2],
+  ["}", 3],
+  [">", 4],
+])
+
+// New function I just learned how to write for MAXIMUM POINT FREE
+const prop =
+  <A, P extends keyof A>(prop: P) =>
+  (a: A) =>
+    a[prop]
 
 const part2 = (rawInput: string) =>
   F.pipe(
@@ -161,9 +193,9 @@ const part2 = (rawInput: string) =>
     parseInput,
     A.map(parseChars),
     A.filter(isIncomplete),
-    A.map((result) =>
-      F.pipe(
-        result.chars,
+    A.map(
+      F.flow(
+        prop("chars"),
         missingChars,
         A.reduce(0, (acc, curr) => acc * 5 + part2Scores.get(curr)!),
       ),
